022924f1b045688bef98c412ba3c07c0aa60d439
Jing Liu <shliujing@foxmail.com>
2019-04-21 01:38:23 +0800

    m

--- siteinfo.js
+++ siteinfo.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: "炫酷相册小程序",
-    uniacid: "18",
-    acid: "18",
+    name: "蒙大教育",
+    uniacid: "2",
+    acid: "2",
     multiid: "0",
     version: "1.0.3",
-    siteroot: "https://www.17878.pw/app/index.php",
+    siteroot: "https://md.iamlj.com/app/index.php",
     design_method: "3"
 };
